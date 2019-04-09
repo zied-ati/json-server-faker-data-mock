e48dd4380ff1bed6b5bef0f712de4e7f9c9be1de
Zied Ati <zied.elati@gmail.com>
2019-04-09 13:18:14 +0100

    update transaction types

--- generate-data.js
+++ generate-data.js
@@ -6,8 +6,9 @@ function generateData () {
     // let eartags = faker.random.number({min: 10000, max: 20000});
     let date = faker.date.between("2019-01-01", "2019-07-31").toISOString().split("T")[0];
     let cpId = faker.random.number({min: 276090000000001, max: 276090000000048});
-    let bsType = faker.random.number({min: 1, max: 5})
-    let type = faker.hacker.phrase();
+    let bsType = faker.random.number({min: 1, max: 5});
+    var transactionTypes = ['buy', 'sell', 'incoming', 'birth', 'death', 'antibiotic'];
+    let type = transactionTypes[Math.floor(Math.random() * transactionTypes.length)];
     let eartags = []
     for (var i = 0; i < 10; i++) { 
       let ear = faker.random.number({min: 1200000000, max: 6000000000});
